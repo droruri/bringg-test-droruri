381da817f9d43aa375ca6e738a706b715113b64f
droruri <droruri@gmail.com>
2019-04-02 23:05:47 +0300

    using new markers components we created before

--- src/components/DriversMap.js
+++ src/components/DriversMap.js
@@ -1,17 +1,15 @@
 import React, {Component} from 'react';
 import {getAllDrivers} from "../state-management/selectors/driver.selector";
 import {connect} from "react-redux";
-import {GoogleApiWrapper, Map, Marker} from 'google-maps-react';
+import {GoogleApiWrapper, Map} from 'google-maps-react';
 import {getAge, getName} from "../state-management/selectors/filter.selector";
-import {getChosenCoordinates, getChosenDriver} from "../state-management/selectors/drivers-map.selector";
-import driverIcon from '../assets/icons8-driver-40.png';
-import taskIcon from '../assets/icons8-flag-filled-40.png';
+import {getChosenCoordinates, getChosenDriver, getChosenTask} from "../state-management/selectors/drivers-map.selector";
 import {getAllTasks} from "../state-management/selectors/tasks.selector";
-import {
-    isAgeAdaptedForDriver,
-    isFilterValueIncludedInDriverName,
-    isTaskAssignedForDriver
-} from "../services/filter.service";
+import {chosenDriverMarker} from "./markers/ChosenDriverMarker";
+import {allDriverMarkers} from "./markers/AllDriverMarkers";
+import {chosenTaskMarker} from "./markers/ChosenTaskMarker";
+import {tasksForChosenDriver} from "./markers/TasksForChosenDriver";
+import {allTasks} from "./markers/AllTasks";
 
 const mapStyles = {
     width: '97%',
@@ -19,48 +17,37 @@ const mapStyles = {
 };
 
 class DriversMapContainer extends Component {
+    state={
+        zoomMap: 16
+    };
+
     render() {
         return (
             <div>
                 <Map
                     google={this.props.google}
-                    zoom={16}
+                    zoom={this.state.zoomMap}
                     style={mapStyles}
                     center={{
                         lat: this.props.coordinates.lat,
                         lng: this.props.coordinates.lng
                     }}>
                     {(this.props.chosenDriver !== null)?
-                        <Marker icon={{url: driverIcon}}
-                                position={{lat: this.props.chosenDriver.latitude, lng: this.props.chosenDriver.longitude}}/>
-                        :this.props.drivers
-                        .filter(driver=>isFilterValueIncludedInDriverName(driver, this.props.name) )
-                        .filter(driver=>isAgeAdaptedForDriver(driver, this.props.age))
-                        .map(driver =>
-                        <Marker key={driver._id}
-                                icon={{url: driverIcon}}
-                                position={{lat: driver.latitude, lng: driver.longitude}}/>
-                    )}
-                    {(this.props.chosenDriver !== null)?
-                        this.props.tasks
-                            .filter(task=>isTaskAssignedForDriver(task, this.props.chosenDriver) )
-                            .map(task =>
-                                <Marker key={task._id}
-                                        icon={{url: taskIcon}}
-                                        position={{lat: task.latitude, lng: task.longitude}}/>)
-                        :this.props.tasks
-                        .filter(task=>isFilterValueIncludedInDriverName(task.assignedDriver, this.props.name) )
-                        .filter(task=>isAgeAdaptedForDriver(task.assignedDriver, this.props.age))
-                        .map(task =>
-                        <Marker key={task._id}
-                                icon={{url: taskIcon}}
-                                position={{lat: task.latitude, lng: task.longitude}}/>
-                    )}
+                        chosenDriverMarker(this.props.chosenDriver):
+                        allDriverMarkers(this.props.drivers, this.props.name, this.props.age)
+                    }
+                    {(this.props.chosenTask !== null)?
+                        chosenTaskMarker(this.props.chosenTask):
+                        (this.props.chosenDriver !== null)?
+                            tasksForChosenDriver(this.props.tasks, this.props.chosenDriver):
+                            allTasks(this.props.tasks, this.props.name, this.props.age)
+                        }
                 </Map>
             </div>
         )
     }
 }
+
 
 const mapStateToProps = state => {
     return {
@@ -69,7 +56,8 @@ const mapStateToProps = state => {
         name: getName(state),
         age: getAge(state),
         coordinates: getChosenCoordinates(state),
-        chosenDriver: getChosenDriver(state)
+        chosenDriver: getChosenDriver(state),
+        chosenTask: getChosenTask(state)
     };
 };
 
