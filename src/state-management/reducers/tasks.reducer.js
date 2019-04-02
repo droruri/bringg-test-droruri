b0b9437f61b7a3bf9c4075ba700400aa0303b500
droruri <droruri@gmail.com>
2019-04-02 23:03:45 +0300

    setting new driver to task in redux store

--- src/state-management/reducers/tasks.reducer.js
+++ src/state-management/reducers/tasks.reducer.js
@@ -1,13 +1,18 @@
-import {LOAD_TASKS} from "../actions/task.actions";
+import {LOAD_TASKS, SET_NEW_DRIVER_FOR_TASK} from "../actions/task.actions";
 
 const initialState = [];
 
 export const tasksReducer = (state = initialState, action) => {
+    let newState = [...state];
     switch (action.type) {
         case LOAD_TASKS:
             return [...action.tasks];
+        case SET_NEW_DRIVER_FOR_TASK:
+            const foundIndex = newState.findIndex(x => x._id === action.task._id);
+            newState[foundIndex] = Object.assign(newState[foundIndex], {assignedDriver: action.driver});
+            return [...newState];
         default:
-            return state
+            return newState;
     }
 };
 
