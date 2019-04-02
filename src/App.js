7a6148d2a9f366ce74f74450a972296b67498827
droruri <droruri@gmail.com>
2019-04-02 09:25:29 +0300

    initial commit for the project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Provider} from 'react-redux'
 import './App.css';
+import {fetchDrivers, fetchTasks} from "./services/fetch-mocks.service";
+import {applyMiddleware, createStore} from "redux";
+import mainReducer from "./state-management/reducers/mainReducer";
+import thunk from "redux-thunk";
+import {loadDriversEffect} from "./state-management/effects/driver/driver.effects";
+import {loadTasksEffect} from "./state-management/effects/tasks/tasks.effects";
+import {MainScreen} from "./screens/MainScreen";
+import {assignTasksForDrivers} from "./services/assign-tasks";
 
 class App extends Component {
+  state = {
+    reduxStore: createStore(mainReducer, applyMiddleware(thunk))
+  };
+
+  componentDidMount() {
+    Promise.all([fetchDrivers(), fetchTasks()])
+        .then((result)=>{
+            const assignedTasksAndDrivers = assignTasksForDrivers(result[0], result[1]);
+            this.state.reduxStore.dispatch(loadDriversEffect(assignedTasksAndDrivers[0]));
+            this.state.reduxStore.dispatch(loadTasksEffect(assignedTasksAndDrivers[1]))
+        });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Provider store={this.state.reduxStore}>
+          <MainScreen/>
+        </Provider>
     );
   }
 }
