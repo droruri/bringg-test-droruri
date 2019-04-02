6f66dd7ea8269b9e52af1874b4bd46205e097f7f
droruri <droruri@gmail.com>
2019-04-02 23:04:57 +0300

    set new action of set a driver to task

--- src/state-management/actions/task.actions.js
+++ src/state-management/actions/task.actions.js
@@ -3,3 +3,10 @@ export const loadTasksSuccess = (tasks) => ({
     type: LOAD_TASKS,
     tasks: tasks
 });
+
+export const SET_NEW_DRIVER_FOR_TASK = 'SET_NEW_DRIVER_FOR_TASK';
+export const setNewDriverForTask = (driver, task) => ({
+    type: SET_NEW_DRIVER_FOR_TASK,
+    driver: driver,
+    task: task
+});
